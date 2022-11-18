9f0170da009d782a7c39a8b9b7f849b9885604b2
Gabriel Binci <gabrielbinci02@gmail.com>
2022-11-18 17:06:44 -0300

    update rofi config

--- .config/awesome/core/autostart.lua
+++ .config/awesome/core/autostart.lua
@@ -1,7 +1,12 @@
 local awful = require("awful")
 
+awful.spawn.with_shell("setxkbmap br abnt2 -option caps:escape") -- keyboard
+
+awful.spawn.with_shell("~/.config/polybar/launch-awesomewm.sh") -- polybar
+
 awful.spawn.with_shell(
 	"picom -b --experimental-backends --animations --animation-window-mass 0.5 --animation-for-open-window zoom --animation-stiffness 350"
 ) -- compositor
-awful.spawn.with_shell("~/.config/polybar/launch.sh") -- polybar
-awful.spawn.with_shell("setxkbmap br abnt2 -option caps:escape")
+
+awful.spawn.with_shell("kmix --keepvisibility")
+awful.spawn.with_shell("nm-applet")
