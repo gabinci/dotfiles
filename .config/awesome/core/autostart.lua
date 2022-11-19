146c1deb278f95eb62a0edb7dcb3fb6ec350bbb9
Gabriel Binci <gabrielbinci02@gmail.com>
2022-11-19 09:21:49 -0300

    change some Awesome / picom stuff

--- .config/awesome/core/autostart.lua
+++ .config/awesome/core/autostart.lua
@@ -1,8 +1,6 @@
 local awful = require("awful")
 
-awful.spawn.with_shell(
-	"picom -b --experimental-backends --animations --animation-window-mass 0.5 --animation-for-open-window zoom --animation-stiffness 350"
-) -- compositor
+awful.spawn.with_shell("picom -b --experimental-backends --animations ") -- compositor
 awful.spawn.with_shell("setxkbmap br abnt2 -option caps:escape") -- keyboard
 
 awful.spawn.with_shell("~/.config/polybar/launch.sh") -- polybar
